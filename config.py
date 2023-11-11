fc3d9631725811354085c4fcce50cd8e34216df1
LxYx <1539357146@qq.com>
2023-11-11 16:41:03 +0800

    None

--- config.py
+++ config.py
@@ -3,10 +3,13 @@
 # 消息中间件
 BROKER_URL = 'redis://:123456@localhost:6379/0'
 # 结果保存后端
-# CELERY_RESULT_BACKEND = 'redis://:123456@localhost:6379/1'
+CELERY_RESULT_BACKEND = 'redis://:123456@localhost:6379/1'
 # 时间区域
 CELERY_TIMEZONE = 'Asia/Shanghai'
 # 工作进程数
 CELERYD_CONCURRENCY = 1
 
 CELERYD_MAX_TASKS_PER_CHILD = 20
+
+CELERY_REJECT_ON_WORKER_LOST = False
+CELERY_ACKS_LATE = True
