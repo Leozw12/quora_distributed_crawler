90dd2e9d2a6d6f3d4e48228df64f5f98342c5cf9
LxYx <1539357146@qq.com>
2023-11-25 12:44:34 +0800

    Upload config

--- config.py
+++ config.py
@@ -7,15 +7,20 @@ BROKER_URL = 'redis://:123456@localhost:6379/0'
 # 时间区域
 CELERY_TIMEZONE = 'Asia/Shanghai'
 # 工作进程数
-CELERYD_CONCURRENCY = 1
+CELERYD_CONCURRENCY = 2
 
-CELERYD_MAX_TASKS_PER_CHILD = 20
+CELERY_BROKER_POOL_LIMIT = 5
 
-CELERY_REJECT_ON_WORKER_LOST = False
+CELERYD_MAX_TASKS_PER_CHILD = 50
+
+CELERY_REJECT_ON_WORKER_LOST = True
 CELERY_ACKS_LATE = True
 
+# CELERY_TASK_REJECT_ON_WORKER_LOST = True
+# CELERY_TASK_ACKS_LATE = True
 
-BROKER_TRANSPORT_OPTIONS = {
-    'socket_timeout': 300,          # 建立连接后的套接字超时
-    'socket_connect_timeout': 30   # 连接Redis服务器的超时
-}
+
+# BROKER_TRANSPORT_OPTIONS = {
+#     'socket_timeout': 300,          # 建立连接后的套接字超时
+#     'socket_connect_timeout': 30   # 连接Redis服务器的超时
+# }
