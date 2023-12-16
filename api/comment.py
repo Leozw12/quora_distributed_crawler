80cf5a16290d19fede03bab666d3298ca18876d7
LxYx <1539357146@qq.com>
2023-12-17 02:06:15 +0800

    Complete multi-level comment crawling.

--- api/comment.py
+++ api/comment.py
@@ -20,3 +20,24 @@ def fetch_comments_by_aid(session: requests.Session, id: str, cursor: int = None
                         'https://www.quora.com/graphql/gql_para_POST?q=AllCommentsListQuery',
                         data=json.dumps(payload), headers=get_quora_header(), cookies=get_quora_cookie()
                         )
+
+
+def fetch_reply_by_comment_id(session: requests.Session, cid: int, cursor: int = None):
+    payload = {
+        "queryName": "CommentRepliesListQuery",
+        "variables": {
+            "commentId": cid,
+            "after": cursor,
+            "first": 100,
+            "requestType": None,
+            "commentType": "answer_comment"
+        },
+        "extensions": {
+            "hash": "65b10f19a7c88a44065f9fce075df7078831c52d3e251fd35eceb8dd3ad950ac"
+        }
+    }
+
+    return session.post( 
+                        'https://www.quora.com/graphql/gql_para_POST?q=CommentRepliesListQuery',
+                        data=json.dumps(payload), headers=get_quora_header(), cookies=get_quora_cookie()
+                        )
--- tasks/answer.py
+++ tasks/answer.py
@@ -4,7 +4,7 @@ from main import app
 from api.answer import fetch_answers_by_qid
 from api.question import fetch_question_info_by_qid
 from api.profile import fetch_user_info_by_uid
-from api.comment import fetch_comments_by_aid
+from api.comment import fetch_comments_by_aid, fetch_reply_by_comment_id
 from utils.logging import log_to_file
 
 
@@ -37,13 +37,14 @@ def fetch_question_with_answer(qid: int) -> None:
         while True:
             response = fetch_answers_by_qid(session, qid, cursor)
 
-            data_connection = response.json()['data']['question']['pagedListDataConnection']
-            
+            data_connection = response.json(
+            )['data']['question']['pagedListDataConnection']
+
             edges = data_connection['edges']
             for edge in edges:
                 node = edge['node']
                 node_type = node['__typename']
-                
+
                 # header meta data
                 if node_type == 'QuestionAnswerHeaderItem':
                     result['numAnswers'] = node['numAnswers']
@@ -54,16 +55,18 @@ def fetch_question_with_answer(qid: int) -> None:
                     answer = node['answer']
                     new_answer = extract_answer(answer)
                     # get answer comments
-                    new_answer.update(comments=get_all_comment(session, answer['id']))
+                    new_answer.update(
+                        comments=get_all_comment(session, answer['id']))
                     result['answers'].append(new_answer)
 
                     # get asker info
                     if result['asker'] == {}:
-                        result['asker'] = fetch_user_info_by_uid(session, answer['question']['asker']['uid'])
+                        result['asker'] = fetch_user_info_by_uid(
+                            session, answer['question']['asker']['uid'])
 
             if not data_connection['pageInfo']['hasNextPage']:
                 break
-            
+
             # update next page cursor
             cursor = data_connection['pageInfo']['endCursor']
 
@@ -77,41 +80,8 @@ def fetch_question_with_answer(qid: int) -> None:
     return result
 
 
-def get_all_comment(session, aid: str):
-    comments = []
-    cursor = None
-
-    while True:
-        response = fetch_comments_by_aid(session, aid, cursor)
-        comments_connection = response.json()['data']['node']['allCommentsConnection']
-
-        for edge in comments_connection['edges']:
-            comment_node = edge['node']
-            user = comment_node['user']
-            comments.append({
-                'commentId': comment_node['commentId'],
-                'creationTime': comment_node['creationTime'],
-                'url': comment_node['url'],
-                'author': {
-                    'uid': user['uid'],
-                    'profileUrl': user['profileUrl'],
-                    'givenName': user['names'][0]['givenName'] if len(user['names']) > 0 else '',
-                    'familyName': user['names'][0]['familyName'] if len(user['names']) > 0 else ''
-                },
-                'content': comment_node['contentQtextDocument']['legacyJson']
-            })
-
-        if not comments_connection['pageInfo']['hasNextPage']:
-            break
-
-        # update next page cursor
-        cursor = comments_connection['pageInfo']['endCursor']
-
-    return comments
-
-
 def extract_answer(answer):
-    """提取出answer中需要的数据"""
+    """Filter out the data we need from the answer data."""
     return {
         'aid': answer['aid'],
         'url': answer['url'],
@@ -132,3 +102,76 @@ def extract_answer(answer):
         'numComments': answer['numDisplayComments'],
         'comments': []
     }
+
+
+def extract_comment(comment):
+    """Filter out the data we need from the comment data."""
+    comment_node = comment['node']
+    user = comment_node['user']
+    return {
+        'commentId': comment_node['commentId'],
+        'creationTime': comment_node['creationTime'],
+        'url': comment_node['url'],
+        'author': {
+            'uid': user['uid'],
+            'profileUrl': user['profileUrl'],
+            'givenName': user['names'][0]['givenName'] if len(user['names']) > 0 else '',
+            'familyName': user['names'][0]['familyName'] if len(user['names']) > 0 else ''
+        },
+        'content': comment_node['contentQtextDocument']['legacyJson']
+    }
+
+
+def get_all_reply(session, cid: int):
+    replys = []
+    cursor = None
+    while True:
+        response = fetch_reply_by_comment_id(session, cid, cursor)
+        replies_connection = response.json(
+        )['data']['comment']['repliesConnection']
+
+        for edge in replies_connection['edges']:
+            reply = extract_comment(edge)
+
+            # Is there a next level reply?
+            if len(edge['node']['repliesConnection']['edges']) > 0:
+                reply.update(comments=get_all_reply(
+                    session, edge['node']['commentId']))
+
+            replys.append(reply)
+
+        if not replies_connection['pageInfo']['hasNextPage']:
+            break
+
+        # update next page cursor
+        cursor = replies_connection['pageInfo']['endCursor']
+
+    return replys
+
+
+def get_all_comment(session, aid: str):
+    comments = []
+    cursor = None
+
+    while True:
+        response = fetch_comments_by_aid(session, aid, cursor)
+        comments_connection = response.json(
+        )['data']['node']['allCommentsConnection']
+
+        for edge in comments_connection['edges']:
+            comment = extract_comment(edge)
+
+            # Is there a next level reply?
+            if len(edge['node']['repliesConnection']['edges']) > 0:
+                comment.update(comments=get_all_reply(
+                    session, edge['node']['commentId']))
+
+            comments.append(comment)
+
+        if not comments_connection['pageInfo']['hasNextPage']:
+            break
+
+        # update next page cursor
+        cursor = comments_connection['pageInfo']['endCursor']
+
+    return comments
