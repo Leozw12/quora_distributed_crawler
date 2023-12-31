a605bf3364caa936129d69b0b095f3cf6d867d8d
LxYx <1539357146@qq.com>
2024-01-01 02:49:00 +0800

    Update server api.

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import subprocess
 from typing import Any
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
@@ -27,21 +28,19 @@ class Question(BaseModel):
     answers: Any
 
 
-def get_directory_size(directory):
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(directory):
-        for filename in filenames:
-            filepath = os.path.join(dirpath, filename)
-            total_size += os.path.getsize(filepath)
-    return total_size
+def get_directory_size_fast(directory):
+    """Get directory size using system command for faster performance."""
+    try:
+        result = subprocess.check_output(['du', '-s', directory])
+        return int(result.split()[0].decode('utf-8')) * 1024  # du returns size in kilobytes
+    except Exception as e:
+        return str(e)
 
 
-# 字节数转换为合适大小单位
 def convert_bytes(byte_size):
-    # 定义单位
+    """Convert bytes to a more readable format."""
     units = ['B', 'KB', 'MB', 'GB', 'TB']
 
-    # 选择合适的单位
     unit_index = 0
     while byte_size > 1024 and unit_index < len(units) - 1:
         byte_size /= 1024.0
@@ -51,19 +50,15 @@ def convert_bytes(byte_size):
     return result
 
 
-def count_small_files(directory):
-    count = 0
-
-    for dirpath, dirnames, filenames in os.walk(directory):
-        for filename in filenames:
-            filepath = os.path.join(dirpath, filename)
-            file_size = os.path.getsize(filepath)
-
-            # 检查文件大小是否小于 1KB
-            if file_size < 1024:
-                count += 1
-
-    return count
+def count_small_files(directory, max_size_kb):
+    """Count files smaller than a certain size using the 'find' command."""
+    try:
+        # Constructing the find command
+        command = ['find', directory, '-type', 'f', '-size', f'-{max_size_kb}k', '|', 'wc', '-l']
+        result = subprocess.check_output(' '.join(command), shell=True)
+        return int(result.strip())
+    except Exception as e:
+        return str(e)
 
 
 @app.post("/upload")
@@ -88,37 +83,28 @@ async def upload_file(file: UploadFile = File(...)):
     return {"message": "Upload successfully."}
 
 
-# @app.get("/list/complete") 
-# async def get_complete_list():
-#     file_list = [file.name for file in Path('./results').glob('*') if file.is_file().]
-#     id_list = []
+@app.get("/list/complete") 
+async def get_complete_list():
+    """Get a list of completed files."""
+    file_list = [int(file.stem) for file in Path('./results').glob('*.json')]
+    return file_list
 
-#     return id_list
 
+@app.get("/info")
+async def get_info():
+    """Get information about the current state of the server."""
+    completion_num = len(await get_complete_list())
+    less_than_1kb = count_small_files('./results', 1)
+    less_than_1kb_ratio = less_than_1kb / completion_num
 
-# @app.get("/info")
-# async def get_info():
-#     rc = redis.Redis(host='localhost', password='hP@9lRv2QxJ_2JreGx', port=6379, db=0)
-#     try:
-#         queue_num = rc.llen('celery')
-#     except redis.RedisError:
-#         queue_num = 0
-#     finally:
-#         rc.connection_pool.disconnect()
-
-#     completion_num = len(await get_complete_list())
-#     less_than_1kb = count_small_files('..')
-#     less_than_1kb_ratio = less_than_1kb / completion_num
-
-#     return {
-#         'queue_num': queue_num,
-#         'completion_num': completion_num,
-#         'completion_size': convert_bytes(get_directory_size('..')),
-#         'less_than_1kb': less_than_1kb,
-#         'less_than_1kb_ratio': round(less_than_1kb_ratio, 4) * 100
-#         }
+    return {
+        'completion_num': completion_num,
+        'completion_size': convert_bytes(get_directory_size_fast('./results')),
+        'less_than_1kb': less_than_1kb,
+        'less_than_1kb_ratio': round(less_than_1kb_ratio, 4) * 100
+        }
 
 
 if __name__ == '__main__':
     Path("./results/").mkdir(parents=True, exist_ok=True)
-    uvicorn.run(app="main:app", host='0.0.0.0', port=65534, reload=True)
+    uvicorn.run(app="main:app", host='0.0.0.0', port=65534, reload=True, workers=2)
