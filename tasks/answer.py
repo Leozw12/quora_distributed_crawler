60b20ca1d5134f7275d9148b47f87ccc0b6e13f2
LxYx <1539357146@qq.com>
2023-12-31 20:32:35 +0800

    Request the session to return to the original scheme.

--- tasks/answer.py
+++ tasks/answer.py
@@ -13,9 +13,9 @@ from utils.session_util import build_session_with_retry
 
 @app.task(bind=True, acks_late=True)
 def fetch_question_with_answer(self, qid: int) -> None:
-    # session: requests.Session = app.conf['session']
+    session: requests.Session = app.conf['session']
     # TODO: Temporary solution, because multiple processes share a session, resulting in the problem of slow request.
-    session: requests.Session = build_session_with_retry()
+    # session: requests.Session = build_session_with_retry()
 
     # output formatte
     result = {
